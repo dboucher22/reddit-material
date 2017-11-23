790527923a8bb10f23a987b4bd6fc17e0ecf6b74
Dane <dvdev8@gmail.com>
2017-11-23 19:16:56 +0200

    style: app shell with some mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+    subreddits: Subreddit[] = [
+        { id: 1, name: '4chan' },
+        { id: 2, name: 'Cats' },
+        { id: 3, name: 'Funny' },
+        { id: 4, name: 'HoldMyBeer' }
+        { id: 5, name: 'Jokes' }
+        { id: 6, name: 'KateBeckinsale' }
+        { id: 7, name: 'NSFW_GIF' }
+        { id: 8, name: 'WatchItForThePlot' }
+    ];
+}
 
+export interface Subreddit {
+    id: number;
+    name: string;
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule
+} from '@angular/material';
 
 const materialImports = [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatMenuModule,
     MatToolbarModule,
     MatSidenavModule
